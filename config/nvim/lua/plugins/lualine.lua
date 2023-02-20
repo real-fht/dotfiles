a295e024d58324c9f85170def61b21f940e96a93
real-fht <nferhat20@gmail.com>
2023-02-20 22:51:36 +0100

    Transparent statusline for neovim

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -46,8 +46,8 @@ M.config = function()
         -- We are going to use lualine_c an lualine_x as left and
         -- right section. Both are highlighted by c theme .  So we
         -- are just setting default looks o statusline
-        normal = { c = { fg = C.statusline, bg = C.statusline } },
-        inactive = { c = { fg = C.statusline, bg = C.statusline } },
+        normal = { c = { fg = C.statusline, bg = "none" } },
+        inactive = { c = { fg = C.statusline, bg = "none" } },
       },
       diasabled_filetypes = { statusline = { "neo-tree" } },
       ignore_focus = { "neo-tree" },
